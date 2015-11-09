8db4a8c8d08c139f61f95bdd746d1d53f81685c6
Arief Fikhrie <arief.fik91@gmail.com>
2015-11-09 19:52:45 +0800

    Fix preventDefault problem as the scroll will go to targeted place then animate. Fix event created every time click triggered

--- animate-anchor.js
+++ animate-anchor.js
@@ -11,34 +11,52 @@
 	function AnimateAnchor(element, options) {
 		this._id = id++;
 		this.element = element;
+		this.target = $($(element).attr('href'))[0];
 		this._defaults = defaults;
 		this.settings = $.extend({}, defaults, options);
 		this.init();
 	}
 	AnimateAnchor.prototype = {
+		anchorTo: function() {
+			alert('anchor to function');
+		},
 		init: function() {
 			var elem = $(this.element),
 				settings = this.settings,
-				target = $(elem.attr('href')),
+				target = $(this.target),
 				location = window.location,
 				afterScrollHandle = function(e) {
+					var elem = this;
+					console.log(elem);
 					elem.trigger('afterScroll.aa');
-				};
+				},
+				doDefault = false;
 
+			elem.on('afterScroll.aa', function(e) {
+				settings.afterScroll(e);
+				doDefault = true;
+				elem.trigger('click');
+			});
 			elem.on('click', function(e) {
 				e.stopPropagation();
+				e.preventDefault();
+				if (!doDefault) {
+					var offset = settings.offset;
+					if ($(settings.header).length > 0) {
+						offset = $(settings.header).height() + offset;
+					}
+					$('html, body').stop().animate({
+						scrollTop: target.offset().top - parseInt(offset, 10)
+					}, settings.duration, settings.easing).promise().done($.proxy(afterScrollHandle, elem));
 
-				var offset = settings.offset;
-				if ($(settings.header).length > 0) {
-					offset = $(settings.header).height() + offset;
+				} else {
+					doDefault = false;
+					var targetID = target.prop('id');
+					target.prop('id', '');
+					location.hash = elem.attr('href');
+					target.prop('id', targetID);
+					// this.anchorTo();
 				}
-				$('html, body').stop().animate({
-					scrollTop: target.offset().top - parseInt(offset, 10)
-				}, settings.duration, settings.easing).promise().done(afterScrollHandle);
-
-				elem.on('afterScroll.aa', function(e){
-					settings.afterScroll(e);
-				});
 			});
 		}
 	};
@@ -60,29 +78,29 @@ $(document).ready(function() {
 			duration = elem.data('animate-duration'),
 			easing = elem.data('animate-easing'),
 			options = {};
-		if ( offset !== undefined && offset !== null && offset !== '' ){
+		if (offset !== undefined && offset !== null && offset !== '') {
 			options.offset = offset;
 		}
-		if ( header !== undefined && header !== null && header !== '' ){
+		if (header !== undefined && header !== null && header !== '') {
 			options.header = header;
 		}
-		if ( duration !== undefined && duration !== null && duration !== '' ){
+		if (duration !== undefined && duration !== null && duration !== '') {
 			options.duration = duration;
 		}
-		if ( easing !== undefined && easing !== null && easing !== '' ){
+		if (easing !== undefined && easing !== null && easing !== '') {
 			options.easing = easing;
 		}
 		elem.animateAnchor(options);
 	});
-	setTimeout( function(){
-		if ( window.location.hash !== '' ){
+	setTimeout(function() {
+		if (window.location.hash !== '') {
 			var hash = window.location.hash,
 				target = $(hash),
 				elem = $('a[href="' + hash + '"]');
 
 			console.log(elem);
 
-			if ( elem.length > 0 ){
+			if (elem.length > 0) {
 				var offset = elem.data('offset-top');
 				if (offset === undefined || offset === null || offset === '') {
 					offset = 75;
